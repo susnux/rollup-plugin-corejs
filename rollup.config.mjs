14243189a3709e4504ab5673f41a1d6f5ee83a6a
Ferdinand Thiessen <rpm@fthiessen.de>
2023-01-29 21:54:12 +0100

    🔨 Add rollup config for building
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Ferdinand Thiessen <rpm@fthiessen.de>
+ *
+ * SPDX-License-Identifier: EUPL-1.2
+ */
+
+import typescript from "@rollup/plugin-typescript"
+import { createRequire } from "node:module"
+
+const require = createRequire(import.meta.url)
+const pkg = require("./package.json")
+
+const external = [
+	...Object.keys(pkg.dependencies),
+	...(pkg.peerDependencies ? Object.keys(pkg.peerDependencies) : []),
+]
+
+const config = (input, output) => ({
+	input,
+	external,
+	plugins: [typescript()],
+	output: [output],
+})
+
+export default [
+	config("./src/index.ts", {
+		file: "dist/index.mjs",
+		format: "esm",
+	}),
+	config("./src/index.ts", {
+		file: "dist/index.js",
+		format: "cjs",
+	}),
+]
